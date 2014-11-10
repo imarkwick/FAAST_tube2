5155f13d2afe7b783dc6a50b3b8ce3095fc38d4e
imarkwick <izzymarkwick@gmail.com>
2014-11-10 23:50:47 +0000

    Train initiated with 2 carriages and can add more

--- lib/train.rb
+++ lib/train.rb
@@ -2,13 +2,17 @@ require_relative 'passenger_holder'
 
 class Train
 
-	# include PassengerHolder
+	include PassengerHolder
 
-	# def initialize()
-	# 	@carriages = [Carriage.new, Carriage.new]
-	# end
+	def initialize()
+		@carriages = [Carriage.new, Carriage.new]
+	end
 
-	# def carriage_count
-	# 	carriages.count
-	# end
+	def carriage_count
+		@carriages.count
+	end
+
+	def add_carriage(carriage)
+		@carriages << Carriage
+	end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -6,9 +6,12 @@ describe Train do
 	let(:train) {Train.new}
 	let(:carriage) {double :carriage}
 
-	# it 'can be initialized with 2 carriages' do
-	# 	expect(train.carriage_count).to eq 2
-	# end
+	it 'can be initialized with 2 carriages' do
+		expect(train.carriage_count).to eq 2
+	end
 
-	# it can add a carriage
+	it 'can add a carriage' do
+		train.add_carriage(carriage)
+		expect(train.carriage_count).to eq 3
+	end
 end
